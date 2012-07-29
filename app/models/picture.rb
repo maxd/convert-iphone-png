56a98981d3b7824e6e4b8958e026eeb65775f016
Maxim Dobryakov <closer.main@gmail.com>
2012-07-30 00:51:18 +0400

    Remove thumbnail creation to improve performance on VPS server

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,9 +2,8 @@ class Picture < ActiveRecord::Base
   attr_accessible :wrong, :png_file
 
   has_attached_file :png_file,
-                    styles: { converted: '', thumb: '200x160>' },
-                    convert_options: { thumb: '-gravity center -background none -extent 210x170 ' },
-                    processors: [ :png_converter, :thumbnail ],
+                    styles: { converted: '' },
+                    processors: [ :png_converter ],
                     url: '/system/png-files/:hash-:style.:extension',
                     hash_secret: 'abacadae'
 
